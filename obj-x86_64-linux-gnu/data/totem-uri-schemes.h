2541e348da521eb627af25488916ae2739256126
vandyyu <vandyyu@zhaoxin.com>
2020-10-20 10:36:29 +0800

    init project.

---
+++ obj-x86_64-linux-gnu/data/totem-uri-schemes.h
@@ -0,0 +1,14 @@
+/* generated with uri-scheme-include.sh, don't edit */
+static const gchar *uri_schemes[] = {
+"pnm",
+"mms",
+"net",
+"rtp",
+"rtmp",
+"rtsp",
+"mmsh",
+"uvox",
+"icy",
+"icyx",
+NULL
+};
---
+++ obj-x86_64-linux-gnu/src/griloresources.h
@@ -0,0 +1,7 @@
+#ifndef __RESOURCE__totem_H__
+#define __RESOURCE__totem_H__
+
+#include <gio/gio.h>
+
+extern GResource *_totem_get_resource (void);
+#endif
